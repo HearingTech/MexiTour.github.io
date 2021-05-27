6afcb0d5d0a9857423ac7838260650713ebce353
HearingTech <84751339+HearingTech@users.noreply.github.com>
2021-05-27 09:30:48 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyB5C626J5zid1rAeUgiiqflTXqVl2Zjm30",
+    authDomain: "authmra3nv41.firebaseapp.com",
+    projectId: "authmra3nv41",
+    storageBucket: "authmra3nv41.appspot.com",
+    messagingSenderId: "25387366690",
+    appId: "1:25387366690:web:452826268b42d6e4694d7b",
+    measurementId: "G-BD9WPZ8R8T"
+  });
