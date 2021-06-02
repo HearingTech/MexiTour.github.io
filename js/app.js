928748bf4d2404ba45a3e253c39826bdb907ac92
HearingTech <84751339+HearingTech@users.noreply.github.com>
2021-06-01 21:37:54 -0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,9 @@
+firebase.initializeApp({
+    apiKey: "AIzaSyCTDRVAbSZyWrckuGPXG9teXLNG7jUmBTo",
+    authDomain: "basepha-2021.firebaseapp.com",
+    projectId: "basepha-2021"
+});
+
+// Initialize Cloud Firestore through Firebase
+
+var db = firebase.firestore();
