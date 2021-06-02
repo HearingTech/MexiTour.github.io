c3bf912cc91e8c511eb3fbd8cd18977d879ebe66
HearingTech <84751339+HearingTech@users.noreply.github.com>
2021-06-01 22:00:10 -0500

    Update R3.js

--- js/R3.js
+++ js/R3.js
@@ -15,7 +15,7 @@ function acceso(){
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
     
-    firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+    firebase.auth.GoogleAuthProvider().catch(function (error) {
   // Handle Errors here.
   var errorCode = error.code;
   var errorMessage = error.message;
