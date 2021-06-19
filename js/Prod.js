d4d3e05c310b8543e42aa1f2ce1664aceb2ca6eb
HearingTech <84751339+HearingTech@users.noreply.github.com>
2021-06-18 23:35:23 -0500

    Update Prod.js

--- js/Prod.js
+++ js/Prod.js
@@ -1,7 +1,7 @@
 firebase.initializeApp({    
-    apiKey: "AIzaSyCTDRVAbSZyWrckuGPXG9teXLNG7jUmBTo",
-    authDomain: "basepha-2021.firebaseapp.com",
-    projectId: "basepha-2021",
+    apiKey: "AIzaSyAHlj_tn8i8m-xTFCGcOKazDQEgFLkzlTo",
+    authDomain: "fir-1502.firebaseapp.com",
+    projectId: "fir-1502"
 });
 // Initialize Cloud Firestore through Firebase
 
